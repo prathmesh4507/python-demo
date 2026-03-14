8a19f4d1b16a73f8a7133252d5766d5f60508e25
Prathmesh Patil <prathmesh@example.com>
2026-03-14 10:27:51 +0530

    new pp

---
+++ pp/demo.py
@@ -0,0 +1,123 @@
+#a = 100
+#b = 20
+#c = 30
+
+#if a>b and a>c:
+ #   print("a is largest number")
+#if b>a and b>c:
+ #       print("B is largest number")
+#if c>a and c>b:
+#            print("c is argest number")
+
+#marks=int(input("enter a number: "))
+##
+#if marks>=90:
+    #print("grade A+")
+#elif marks>80:
+ #   print("Grade B+")
+#elif marks>50 :
+ #   print("Grade C+")
+#else:
+   ## print("fail")
+
+#area of rectangle
+
+#length=(input("enter a no.1:"))
+#hight=(input("enter a no.2:"))  
+
+#length=int(length)
+#hight=int(hight)
+#area_of_rect= length*hight
+#print(area_of_rect)
+
+#FIND THE MAX NO.   
+
+#a=int(input("enter the no.1:"))
+#b=int(input("enter the no.2:"))
+
+#if a>b:
+ #   print("a is greater than b")
+#else:
+ #   print("b is greater than a")
+
+#ind the max no. among three no.
+
+#a =int (input("enter a no.1:"))
+#b =int (input("enter a no.2:"))
+#c =int (input("enter a no.3:"))
+
+#if a>=b and a>=c:
+ #   print("a is greter no.")
+#elif b>a and b>c:
+ #   print("b  is  greater no.")
+#else:
+ #   print("c is greater no.")
+
+#choice
+#choice=input("do you want to continue (y/n): ")
+
+#  if choice =="y":
+  #   print("you have choose yes")
+#elif choice =="n":
+ #    print("you have choose No")
+#else:
+ #    print("invalid choice")
+
+#enterd two no.s
+#a=int(input("enter a no.1:"))
+#b=int(input("enter a no.2:"))
+
+#add=print("1.add")
+#sub=print("2.sub")
+#mul=print("3.mul")
+#div=print("4.div")
+ 
+#operation=(input("enter operation"))
+#print(operation)
+
+#if operation==1 or operation=="add":
+ #   result=a+b
+  #  print("addition",result)
+
+#elif operation==2:
+ #   result=a-b
+  #  print("substraction:",result)
+#elif operation==3:
+ #   result=a*b
+  #  print("multiply",result)
+#elif operation==4:
+ #   result=a/b
+  #  print("division",result)
+#else:
+ #   print("cannot divide by zero")
+
+#ATM programm
+#import pyfiglet
+#balance=2000
+#print("Welcome To ATM")
+#name=input("Enter your name")
+#print(pyfiglet.print_figlet(name))
+#print("1.check balance")
+#print("2.cash_withdraw")
+#print("3.deposite")
+#print("4. exit")
+
+#option=input("enter option")
+#print(option)
+
+#if option=="1" or option=="check balance":
+ #   print("Your balance is:",balance)
+    
+#elif option=="2" or option=="cash_withdraw":
+ #   print("you have selecte withdraw")
+  # with_amount=(balance-amount)
+   # print(with_amount)
+#elif option=="3"  or option=="deposite":
+ #   print("you have select depsoite")
+  #  deposite=int(input("your amount"))
+   # balance = deposite
+    #print("your new balance is:",balance )
+#elif option=="4" or option=="exit":
+ #   print("Thank you for using ATM")
+
+    
